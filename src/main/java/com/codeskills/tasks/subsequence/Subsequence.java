47527ede5aa2bc8b40b0ac97941733fb119b8471
joche <mail@jochenkappes.de>
2020-11-10 09:02:43 +0100

    Subsequenze solved

--- src/main/java/com/codeskills/tasks/subsequence/Subsequence.java
+++ src/main/java/com/codeskills/tasks/subsequence/Subsequence.java
@@ -1,5 +1,6 @@
 package com.codeskills.tasks.subsequence;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class Subsequence {
@@ -14,6 +15,40 @@ public class Subsequence {
     @SuppressWarnings("rawtypes")
     public boolean find(List x, List y) {
         // TODO: Implement the logic here
+
+        //int gefunden = 0;
+        int i = 0;
+
+        if (x == null || y == null) {
+            throw new IllegalArgumentException("List cannot be null!");
+        }
+
+
+        if (x.isEmpty() == true)
+                return true;
+
+        if (x.size() > y.size())
+            return false;
+
+       for (Object oneY : y) {
+
+           System.out.println("Aktuell Y ist : " + oneY);
+           if (oneY.equals(x.get(i))){
+
+                if (i < x.size() -1) {
+                   System.out.println("gefunden " + x.get(i) + " at: " + i);
+                   i++;
+                }
+
+               if (i == x.size()-1){
+                   System.out.println("letzter gefunden " + x.get(i) + " at: " + i);
+                   return true;
+               }
+
+           }
+           System.out.println("=====================");
+        }
+
         return false;
     }
 }
